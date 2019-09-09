9e0fb801ee3495bbf666c23c6120fcc2792cfb68
xingleilei <xingleilei@flashhold.com>
2019-09-09 13:18:46 +0800

    上传文件论文

---
+++ 04PIBOT/原理图/电机驱动模块/TB6612FNG驱动模块/Arduino TB6612FNG测试程序/tb6612fng_test/tb6612fng_test.ino
@@ -0,0 +1,71 @@
+/****************************************************************************8
+   亚博智能科技有限公司
+   程序：TB6612FNG测试程序
+   程序直接烧录到平衡车上就可使用测试电机
+*/
+int STBY = 8; //使能端
+  
+//Motor A
+int PWMA = 9; //左电机PWM输出控制脚   
+int AIN1 = 7; //左电机正极  
+int AIN2 = 6; //左电机负极  
+  
+//Motor B  
+int PWMB = 10; //右电机PWM输出控制脚
+int BIN1 = 13; //右电机正极 
+int BIN2 = 12; //右电机负极  
+  
+void setup(){  
+  pinMode(STBY, OUTPUT);  
+  
+  pinMode(PWMA, OUTPUT);  
+  pinMode(AIN1, OUTPUT);  
+  pinMode(AIN2, OUTPUT);  
+  
+  pinMode(PWMB, OUTPUT);  
+  pinMode(BIN1, OUTPUT);  
+  pinMode(BIN2, OUTPUT);  
+}  
+
+void runset(int motor, int speed, int direction){  
+  
+  digitalWrite(STBY, HIGH); //使能驱动模块脚 
+  
+  boolean Pin1 = LOW;  
+  boolean Pin2 = HIGH;  
+  
+  if(direction == 1){  
+    Pin1 = HIGH;  
+    Pin2 = LOW;  
+  }  
+  
+  if(motor == 1){  
+    digitalWrite(AIN1, Pin1);  
+    digitalWrite(AIN2, Pin2);  
+    analogWrite(PWMA, speed);  
+  }else{  
+    digitalWrite(BIN1, Pin1);  
+    digitalWrite(BIN2, Pin2);  
+    analogWrite(PWMB, speed);  
+  }  
+}  
+  
+void loop(){  
+  runset(1, 255, 1); //左电机全速向前转  
+  runset(2, 255, 1); //右电机全速向前转  
+  
+  delay(1000); //1秒 
+  stop(); //停转
+  delay(250); //两轮继续向前转  
+  
+  runset(1, 128, 0); //左电机半速向后转  
+  runset(2, 128, 0); //右电机半速向后转  
+  
+  delay(1000);  
+  stop();  
+  delay(250);  
+}  
+   
+void stop(){  
+  digitalWrite(STBY, LOW);   
+}  
